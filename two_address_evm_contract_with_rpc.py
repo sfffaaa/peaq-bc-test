820ce5f9a512797be459f3e1382cac9a1a8c0431
Jay Pan <sfffaaa@gmail.com>
2022-02-04 16:21:28 +0100

    Update test case because sometimes the txs fail

--- two_address_evm_contract_with_rpc.py
+++ two_address_evm_contract_with_rpc.py
@@ -2,7 +2,7 @@ import sys
 import json
 
 from substrateinterface import SubstrateInterface, Keypair, KeypairType
-from utils import TOKEN_NUM_BASE, show_extrinsic, SCALE_CODEC
+from utils import show_extrinsic, SCALE_CODEC
 from web3 import Web3
 
 import pprint
@@ -23,36 +23,6 @@ MNEMONIC = [
 ]
 
 
-def show_account(substrate, addr, out_str):
-    result = substrate.query("System", "Account", [addr])
-    print(f'{out_str} {addr}')
-    pp.pprint(result.value)
-    print('')
-    return result.value['data']['free']
-
-
-def transfer(substrate, kp_src, kp_dst_addr, token_num):
-    nonce = substrate.get_account_nonce(kp_src.ss58_address)
-
-    call = substrate.compose_call(
-        call_module='Balances',
-        call_function='transfer',
-        call_params={
-            'dest': kp_dst_addr,
-            'value': token_num * TOKEN_NUM_BASE
-        })
-
-    extrinsic = substrate.create_signed_extrinsic(
-        call=call,
-        keypair=kp_src,
-        era={'period': 64},
-        nonce=nonce
-    )
-
-    receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
-    show_extrinsic(receipt, 'transfer')
-
-
 def call_eth_transfer_a_lot(substrate, kp_src, eth_src, eth_dst):
     nonce = substrate.get_account_nonce(kp_src.ss58_address)
 
@@ -63,9 +33,9 @@ def call_eth_transfer_a_lot(substrate, kp_src, eth_src, eth_dst):
             'source': eth_src,
             'target': eth_dst,
             'input': '0x',
-            'value': '0xfffffffffffffffffffffffffffff00000000000000000000000000000000000',
+            'value': '0xffffffffffffffffffffffffffffff0000000000000000000000000000000000',
             'gas_limit': 4294967294,
-            'max_fee_per_gas': "0xfffff00000000000000000000000000000000000000000000000000000000000",
+            'max_fee_per_gas': "0xfffffff000000000000000000000000000000000000000000000000000000000",
             'max_priority_fee_per_gas': None,
             'nonce': None,
             'access_list': []
@@ -86,74 +56,19 @@ def call_eth_transfer_a_lot(substrate, kp_src, eth_src, eth_dst):
         raise IOError
 
 
-def transfer_erc_token(substrate, kp_src, eth_src, eth_dst, contract_addr):
-    nonce = substrate.get_account_nonce(kp_src.ss58_address)
-
-    call = substrate.compose_call(
-        call_module='EVM',
-        call_function='call',
-        call_params={
-            'target': contract_addr,
-            'source': eth_src,
-            'input': f'0xa9059cbb000000000000000000000000{eth_dst.lower()[2:]}00000000000000000000000000000000000000000000000000000000000000{hex(ERC_TOKEN_TRANSFER)[2:]}',
-            'value': '0x0000000000000000000000000000000000000000000000000000000000000000',
-            'gas_limit': 4294967295,
-            'max_fee_per_gas': "0xfffffff000000000000000000000000000000000000000000000000000000000",
-            'max_priority_fee_per_gas': None,
-            'nonce': None,
-            'access_list': []
-        })
-
-    extrinsic = substrate.create_signed_extrinsic(
-        call=call,
-        keypair=kp_src,
-        era={'period': 64},
-        nonce=nonce
-    )
-
-    receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
-    show_extrinsic(receipt, 'call')
-
-    if not receipt.is_success:
-        print(substrate.get_events(receipt.block_hash))
-        raise IOError
-    #  else:
-        #  created_event = [_ for _ in substrate.get_events(receipt.block_hash)
-        #                   if _['event'].value['event_id'] == 'Created'][0]
-        #  return created_event.value['attributes']
-
-
-def withdraw(substrate, kp_src, kp_dst):
-    nonce = substrate.get_account_nonce(kp_src.ss58_address)
-
-    call = substrate.compose_call(
-        call_module='EVM',
-        call_function='withdraw',
-        call_params={
-            'address': kp_src.ss58_address,
-            'value': 1000
-        })
-
-    extrinsic = substrate.create_signed_extrinsic(
-        call=call,
-        keypair=kp_src,
-        era={'period': 64},
-        nonce=nonce
-    )
-
-    receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
-    show_extrinsic(receipt, 'evm_withdraw')
-    print(substrate.get_events(receipt.block_hash))
-
-
 def send_eth_token(w3, kp_src, kp_dst, token_num):
     nonce = w3.eth.getTransactionCount(kp_src.ss58_address)
+    # gas = web3.toWei(Decimal('0.000000005'), 'ether')
+    gas = 4294967294
+    price = 1000
+    print(token_num + gas * price)
     tx = {
         'from': kp_src.ss58_address,
         'to': kp_dst.ss58_address,
         'value': token_num,
-        'gas': 4294967294,
-        'gasPrice': 100000,
+        'gas': gas,
+        'maxFeePerGas': w3.toWei(250, 'gwei'),
+        'maxPriorityFeePerGas': w3.toWei(2, 'gwei'),
         'nonce': nonce,
         'chainId': CHAIN_ID
     }
@@ -163,6 +78,7 @@ def send_eth_token(w3, kp_src, kp_dst, token_num):
     if tx_receipt['status'] != 1:
         print(tx_receipt)
         raise IOError
+    print('✅ send_eth_token, Success')
 
 
 def deploy_contract(w3, kp_src):
@@ -178,7 +94,8 @@ def deploy_contract(w3, kp_src):
         bytecode=bytecode).constructor().buildTransaction({
             'from': kp_src.ss58_address,
             'gas': 4294967294,
-            'gasPrice': 100000,
+            'maxFeePerGas': w3.toWei(250, 'gwei'),
+            'maxPriorityFeePerGas': w3.toWei(2, 'gwei'),
             'nonce': nonce,
             'chainId': CHAIN_ID})
 
@@ -189,6 +106,7 @@ def deploy_contract(w3, kp_src):
     address = tx_receipt['contractAddress']
     if not address:
         raise IOError('Contract deploy fails')
+    print('✅ deploy, Success')
     return address
 
 
@@ -204,7 +122,8 @@ def call_copy(w3, address, kp_src):
     tx = contract.functions.callDatacopy(HEX_STR).buildTransaction({
         'from': kp_src.ss58_address,
         'gas': 4294967294,
-        'gasPrice': 100000,
+        'maxFeePerGas': w3.toWei(250, 'gwei'),
+        'maxPriorityFeePerGas': w3.toWei(2, 'gwei'),
         'nonce': nonce,
         'chainId': CHAIN_ID})
 
@@ -214,6 +133,7 @@ def call_copy(w3, address, kp_src):
 
     data = contract.functions.memoryStored().call()
     assert(data.hex() == HEX_STR)
+    print('✅ call_copy, Success')
 
 
 def evm_test():
@@ -235,13 +155,13 @@ def evm_test():
             assert(block['number'] != 0)
 
             kp_eth_dst = Keypair.create_from_mnemonic(MNEMONIC[1], crypto_type=KeypairType.ECDSA)
-            # src_eth_balance = w3.eth.get_balance(kp_eth_src.ss58_address)
-            # send_eth_token(w3, kp_eth_src, kp_eth_dst, int(src_eth_balance) + 1)
 
-            token_num = 1000
+            token_num = 10000000
             dst_eth_before_balance = w3.eth.get_balance(kp_eth_dst.ss58_address)
             print(f'before, dst eth: {dst_eth_before_balance}')
             # Call eth transfer
+            src_eth_balance = w3.eth.get_balance(kp_eth_src.ss58_address)
+            print(f'src eth: {src_eth_balance}')
             send_eth_token(w3, kp_eth_src, kp_eth_dst, token_num)
             dst_eth_after_balance = w3.eth.get_balance(kp_eth_dst.ss58_address)
             print(f'after, dst eth: {dst_eth_after_balance}')
