62cef9266324c1a26604fcd2a1cc2f92bfb34283
Syed Kamran <syed@etolabs.io>
2022-11-15 16:22:17 +0500

    Restructed and added more test for success path

--- test.py
+++ test.py
@@ -6,22 +6,17 @@ from tools.two_address_substrate_with_extrinsic import pallet_did_test
 from tools.two_address_evm_contract_with_extrinsic import evm_extrinsic_test
 from tools.two_address_evm_contract_with_rpc import evm_rpc_test
 from tools.block_creation_time_test import block_creation_time_test
-from tools.pallet_utility_test import pallet_utility_batch_test
-from tools.pallet_utility_test import pallet_utility_batch_all_test
 # from tools.pallet_asset_evm_with_rpc import pallet_asset_evm_test
-
+from tools.pallet_utility_test import pallet_utility_test
 
 if __name__ == '__main__':
     # pallet_batchall_test()
-    #pallet_multisig_test()
-    #pallet_transaction_test()
-    #pallet_did_test()
-
-    # # pallet_assets_test()
-    #evm_rpc_test()
-    #evm_extrinsic_test()
-    # # pallet_asset_evm_test()
-    #block_creation_time_test()
-    pallet_utility_batch_test()
-    pallet_utility_batch_all_test()
-
+    pallet_multisig_test()
+    pallet_transaction_test()
+    pallet_did_test()
+    # pallet_assets_test()
+    evm_rpc_test()
+    evm_extrinsic_test()
+    # pallet_asset_evm_test()
+    block_creation_time_test()
+    pallet_utility_test()
--- tools/pallet_utility_test.py
+++ tools/pallet_utility_test.py
@@ -1,57 +1,164 @@
-import sys
-sys.path.append('./')
 from substrateinterface import SubstrateInterface, Keypair
-from tools.utils import show_extrinsic, WS_URL, TOKEN_NUM_BASE,transfer
+from tools.utils import show_extrinsic, WS_URL, TOKEN_NUM_BASE
 from tools.two_address_substrate_with_extrinsic import show_account
 
+# The purpose of this code is to test pallet_utility functionality
 
-#The purpose of these tests is to test utility pallet functionality
-
-substrate = SubstrateInterface (
+# deinfe a conneciton with a peaq-network node
+substrate = SubstrateInterface(
         url=WS_URL
     )
 
-#source account
+# source account
 kp_src = Keypair.create_from_uri('//Alice')
-#destination account
-kp_dst=Keypair.create_from_uri('//Charlie')
+# destination account
+kp_dst = Keypair.create_from_uri('//Charlie')
+# An arbitrary amount to be transfered from source to destination
+amount_to_be_transfered = 1
 
-#One vaid and one invalid transaciton will added to pallet::utility::batch for execution
-# The restul of valid transaction will not be reverted due to an invalid transaton int batch      
-def pallet_utility_batch_test():
-    
-    print('---- pallet_utility_batch_test!! ----')
-    print('---- ------------------------------- -----')   
-    print() 
-    
+# a valid funds transfer transaction from src to dest with batch
+# after transaction, dest will be credited twice as amount_to_be_transfered
 
-    print("Account balances before transactions:")
-    bal_src_before=show_account(substrate, kp_src.ss58_address, "Source")  
-    bal_dst_before=show_account(substrate, kp_dst.ss58_address, "Destination")  
-        
-     
+
+def all_valid_extrinsics_bath():
+    print('---Start of test: all_valid_extrinsics_bath()---')
+    print()
+
+    # check account balances before transactions
+    bal_src_before = show_account(substrate,
+                                  kp_src.ss58_address, "src bal before trans")
+    bal_dst_before = show_account(substrate,
+                                  kp_dst.ss58_address, "Dest bal before trans")
     nonce = substrate.get_account_nonce(kp_src.ss58_address)
-    amount_to_be_transfered=1
-    
-    #first valid transaciton
+
+    # a valid  transaciton
     payload_first = substrate.compose_call(
         call_module='Balances',
         call_function='transfer',
         call_params={
             'dest': kp_dst.ss58_address,
-            'value': amount_to_be_transfered *TOKEN_NUM_BASE
+            'value': amount_to_be_transfered * TOKEN_NUM_BASE
         })
 
-    #Second invalid transaciton
+    # above valid transaciton repeated twice to compose a bath of transaction
+    batch = substrate.compose_call(
+        call_module='Utility',
+        call_function='batch',
+        call_params={
+            'calls': [payload_first.value, payload_first.value],
+        })
+
+    extrinsic = substrate.create_signed_extrinsic(call=batch,
+                                                  keypair=kp_src,
+                                                  era={'period': 64},
+                                                  nonce=nonce)
+
+    receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
+
+    if not receipt.is_success:
+        print(substrate.get_events(receipt.block_hash))
+        raise IOError
+
+    # check account balances after transaciton
+    bal_src_after = show_account(substrate,
+                                 kp_src.ss58_address, "Src bal after trans")
+    bal_dst_after = show_account(substrate,
+                                 kp_dst.ss58_address, "Dest bal after trans")
+
+    show_extrinsic(receipt, 'batch')
+    # since same amount has been transfered two times
+    assert (bal_dst_before+(amount_to_be_transfered * TOKEN_NUM_BASE) * 2
+            == bal_dst_after)
+
+    print('---End   of test: all_valid_extrinsics_bath()---')
+    print()
+
+
+def all_valid_extrinsics_bath_all():
+    print('---Start of test: all_valid_extrinsics_bath_all()---')
+    print()
+
+    # check account balances before transactions
+    bal_src_before = show_account(substrate,
+                                  kp_src.ss58_address, "src bal before trans")
+    bal_dst_before = show_account(substrate,
+                                  kp_dst.ss58_address, "Dest bal before trans")
+    nonce = substrate.get_account_nonce(kp_src.ss58_address)
+
+    # a valid  transaciton
+    payload_first = substrate.compose_call(
+        call_module='Balances',
+        call_function='transfer',
+        call_params={
+            'dest': kp_dst.ss58_address,
+            'value': amount_to_be_transfered * TOKEN_NUM_BASE
+        })
+
+    # above valid transaciton repeated twice to compose a bath_all of trans
+    batch = substrate.compose_call(
+        call_module='Utility',
+        call_function='batch_all',
+        call_params={
+            'calls': [payload_first.value, payload_first.value],
+        })
+
+    extrinsic = substrate.create_signed_extrinsic(call=batch,
+                                                  keypair=kp_src,
+                                                  era={'period': 64},
+                                                  nonce=nonce)
+
+    receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
+
+    if not receipt.is_success:
+        print(substrate.get_events(receipt.block_hash))
+        raise IOError
+
+    # check account balances after transaciton
+    bal_src_after = show_account(substrate,
+                                 kp_src.ss58_address, "Src bal after trans")
+    bal_dst_after = show_account(substrate,
+                                 kp_dst.ss58_address, "Dest bal after trans")
+
+    show_extrinsic(receipt, 'batch')
+    # since same amount has been transfered two times
+    assert (bal_dst_before+(amount_to_be_transfered * TOKEN_NUM_BASE) * 2
+            == bal_dst_after)
+
+    print('---End   of test: all_valid_extrinsics_bath_all()---')
+    print()
+
+
+def atleast_one_invalid_extrinsic_bath():
+    print('---Start of test: atleast_one_invalid_extrinsic_bath()---')
+    print()
+
+    # check account balances before transactions
+    bal_src_before = show_account(substrate,
+                                  kp_src.ss58_address, "src bal before trans")
+    bal_dst_before = show_account(substrate,
+                                  kp_dst.ss58_address, "Dest bal before trans")
+    nonce = substrate.get_account_nonce(kp_src.ss58_address)
+
+    # a valid  transaciton
+    payload_first = substrate.compose_call(
+        call_module='Balances',
+        call_function='transfer',
+        call_params={
+            'dest': kp_dst.ss58_address,
+            'value': amount_to_be_transfered * TOKEN_NUM_BASE
+        })
+
+    # Second invalid transaciton
     payload_second = substrate.compose_call(
         call_module='Balances',
         call_function='force_transfer',
         call_params={
-            'source': kp_src.ss58_address, 
+            'source': kp_src.ss58_address,
             'dest': kp_dst.ss58_address,
-            'value': amount_to_be_transfered  * TOKEN_NUM_BASE
+            'value': amount_to_be_transfered * TOKEN_NUM_BASE
         })
 
+    # batch of valid and atleast one iinvalid transactionss
     batch = substrate.compose_call(
         call_module='Utility',
         call_function='batch',
@@ -59,60 +166,62 @@ def pallet_utility_batch_test():
             'calls': [payload_first.value, payload_second.value],
         })
 
-    extrinsic = substrate.create_signed_extrinsic(
-        call=batch,
-        keypair= kp_src,
-        era={'period': 64},
-        nonce=nonce
-    )
+    extrinsic = substrate.create_signed_extrinsic(call=batch,
+                                                  keypair=kp_src,
+                                                  era={'period': 64},
+                                                  nonce=nonce)
 
     receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
-            
     if not receipt.is_success:
         print(substrate.get_events(receipt.block_hash))
         raise IOError
-    
-    print("Account balances after transactions:")
-    bal_src_after=show_account(substrate, kp_src.ss58_address, "Source")  
-    bal_dst_after=show_account(substrate, kp_dst.ss58_address, "Destination")  
+
+    # check account balances after transaciton
+    bal_src_after = show_account(substrate,
+                                 kp_src.ss58_address, "Src bal after trans")
+    bal_dst_after = show_account(substrate,
+                                 kp_dst.ss58_address, "Dest bal after trans")
 
     show_extrinsic(receipt, 'batch')
-    assert(bal_dst_before+(amount_to_be_transfered*TOKEN_NUM_BASE)==bal_dst_after)
+    # since amount has been transfered only once
+    assert (bal_dst_before+(amount_to_be_transfered * TOKEN_NUM_BASE)
+            == bal_dst_after)
 
-#One vaid and one invalid transaciton will added to pallet::utility::batch_all for execution
-# The restul of valid transaction will be reverted since batch_all execute the transactions atomically      
-def pallet_utility_batch_all_test():
-    
-    print('---- pallet_utility_batch_all_test!! ----')
-    print('---- ------------------------------- -----')   
-    print()     
+    print('---End   of test: atleast_one_invalid_extrinsics_bath()---')
+    print()
 
-    print("Account balances before transactions:")
-    bal_src_before=show_account(substrate, kp_src.ss58_address, "Source")  
-    bal_dst_before=show_account(substrate, kp_dst.ss58_address, "Destination")  
-             
+
+def atleast_one_invalid_extrinsic_bath_all():
+    print('---Start of test: atleast_one_invalid_extrinsics_bath_all()---')
+    print()
+
+    # check account balances before transactions
+    bal_src_before = show_account(substrate,
+                                  kp_src.ss58_address, "src bal before trans")
+    bal_dst_before = show_account(substrate,
+                                  kp_dst.ss58_address, "Dest bal before trans")
     nonce = substrate.get_account_nonce(kp_src.ss58_address)
-    amount_to_be_transfered=1
-    
-    #first valid transaciton
+
+    # a valid  transaciton
     payload_first = substrate.compose_call(
         call_module='Balances',
         call_function='transfer',
         call_params={
             'dest': kp_dst.ss58_address,
-            'value': amount_to_be_transfered *TOKEN_NUM_BASE
+            'value': amount_to_be_transfered * TOKEN_NUM_BASE
         })
 
-    #Second invalid transaciton
+    # Second invalid transaciton
     payload_second = substrate.compose_call(
         call_module='Balances',
         call_function='force_transfer',
         call_params={
-            'source': kp_src.ss58_address, 
+            'source': kp_src.ss58_address,
             'dest': kp_dst.ss58_address,
-            'value': amount_to_be_transfered  * TOKEN_NUM_BASE
+            'value': amount_to_be_transfered * TOKEN_NUM_BASE
         })
 
+    # batch of valid and atleast one iinvalid transactionss
     batch = substrate.compose_call(
         call_module='Utility',
         call_function='batch_all',
@@ -120,22 +229,29 @@ def pallet_utility_batch_all_test():
             'calls': [payload_first.value, payload_second.value],
         })
 
-    extrinsic = substrate.create_signed_extrinsic(
-        call=batch,
-        keypair= kp_src,
-        era={'period': 64},
-        nonce=nonce
-    )
+    extrinsic = substrate.create_signed_extrinsic(call=batch,
+                                                  keypair=kp_src,
+                                                  era={'period': 64},
+                                                  nonce=nonce)
 
     receipt = substrate.submit_extrinsic(extrinsic, wait_for_inclusion=True)
-              
-    print("Account balances after transactions:")
-    bal_src_after=show_account(substrate, kp_src.ss58_address, "Source")  
-    bal_dst_after=show_account(substrate, kp_dst.ss58_address, "Destination")  
+
+    # check account balances after transaciton
+    bal_src_after = show_account(substrate,
+                                 kp_src.ss58_address, "Src bal after trans")
+    bal_dst_after = show_account(substrate,
+                                 kp_dst.ss58_address, "Dest bal after trans")
 
     show_extrinsic(receipt, 'batch')
-    #destination account balance shoud remain same before and after the transaction
-    assert(bal_dst_before==bal_dst_after)
+    # since due to an invalid transation, all transactions will be reverted
+    assert (bal_dst_before == bal_dst_after)
+
+    print('---End of test: atleast_one_invalid_extrinsics_bath_all()---')
+    print()
 
 
-
+def pallet_utility_test():
+    all_valid_extrinsics_bath()
+    all_valid_extrinsics_bath_all()
+    atleast_one_invalid_extrinsic_bath()
+    atleast_one_invalid_extrinsic_bath_all()
