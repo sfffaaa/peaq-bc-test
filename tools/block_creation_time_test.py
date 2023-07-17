2e8a68acc1688c09ede795e07c2cfbf3c9462ef4
jaypan <pan@peaq.io>
2023-07-17 19:25:55 +0200

    Move block_creation_time_test

--- tools/block_creation_time_test.py
+++ tools/block_creation_time_test.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('./')
+import unittest
 
 from substrateinterface import SubstrateInterface
 from tools.utils import WS_URL
@@ -10,20 +11,9 @@ BLOCK_CREATION_MS = 12000
 BLOCK_TOLERATE_PERCENTAGE = 10
 
 
-def block_creation_time_test():
-    print('--- Test block creation time ---')
-    try:
+class TestBlockCreationTime(unittest.TestCase):
+    def test_block_creation_time(self):
         with SubstrateInterface(url=WS_URL) as substrate:
             ave_time = get_block_creation_times(substrate, BLOCK_TRAVERSE)
-            if abs(ave_time - BLOCK_CREATION_MS) / float(BLOCK_CREATION_MS) * 100. > BLOCK_TOLERATE_PERCENTAGE:
-                print(f'The average block time {ave_time} is longer than the tolerate rate {BLOCK_TOLERATE_PERCENTAGE} * {BLOCK_CREATION_MS}')
-                raise IOError(f'Check the average block creation time {ave_time}')
-            print(f'The block creation time {ave_time} (ms) is okay')
-
-    except ConnectionRefusedError:
-        print("⚠️ No local Substrate node running, try running 'start_local_substrate_node.sh' first")
-        sys.exit()
-
-
-if __name__ == '__main__':
-    block_creation_time_test()
+            self.assertLess(abs(ave_time - BLOCK_CREATION_MS) / float(BLOCK_CREATION_MS) * 100.,
+                            BLOCK_TOLERATE_PERCENTAGE)
