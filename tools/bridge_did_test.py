90004b8b32e1c00ee0776fd26fe4c52b505acffa
jaypan <pan@peaq.io>
2023-07-17 19:58:45 +0200

    Move bridge_did_test

--- tools/bridge_did_test.py
+++ tools/bridge_did_test.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append('./')
+import unittest
 
 from substrateinterface import SubstrateInterface, Keypair, KeypairType
 from tools.utils import transfer, calculate_evm_account, calculate_evm_addr, SCALE_CODEC
@@ -23,118 +24,106 @@ VALIDITY = 1000
 ABI_FILE = 'ETH/did/did.sol.json'
 
 
-def _eth_add_attribute(w3, contract, eth_kp_src, kp_src, key, value):
-    nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
-    tx = contract.functions.add_attribute(kp_src.public_key, key, value, VALIDITY).build_transaction({
-        'from': eth_kp_src.ss58_address,
-        'gas': GAS_LIMIT,
-        'maxFeePerGas': w3.to_wei(250, 'gwei'),
-        'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
-        'nonce': nonce,
-        'chainId': ETH_CHAIN_ID})
+class TestBridgeDid(unittest.TestCase):
+    def _eth_add_attribute(self, contract, eth_kp_src, kp_src, key, value):
+        w3 = self.w3
+        nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
+        tx = contract.functions.add_attribute(kp_src.public_key, key, value, VALIDITY).build_transaction({
+            'from': eth_kp_src.ss58_address,
+            'gas': GAS_LIMIT,
+            'maxFeePerGas': w3.to_wei(250, 'gwei'),
+            'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
+            'nonce': nonce,
+            'chainId': ETH_CHAIN_ID})
 
-    signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    if tx_receipt['status'] != 1:
-        print(tx_receipt)
-        raise IOError
-    print('✅ eth_add_attribute, Success')
-    return tx_receipt['blockNumber']
+        signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
+        tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        self.assertEqual(tx_receipt['status'], 1)
+        print('✅ eth_add_attribute, Success')
+        return tx_receipt['blockNumber']
 
+    def _eth_update_attribute(self, contract, eth_kp_src, kp_src, key, value):
+        w3 = self.w3
+        nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
+        tx = contract.functions.update_attribute(kp_src.public_key, key, value, VALIDITY).build_transaction({
+            'from': eth_kp_src.ss58_address,
+            'gas': GAS_LIMIT,
+            'maxFeePerGas': w3.to_wei(250, 'gwei'),
+            'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
+            'nonce': nonce,
+            'chainId': ETH_CHAIN_ID})
 
-def _eth_update_attribute(w3, contract, eth_kp_src, kp_src, key, value):
-    nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
-    tx = contract.functions.update_attribute(kp_src.public_key, key, value, VALIDITY).build_transaction({
-        'from': eth_kp_src.ss58_address,
-        'gas': GAS_LIMIT,
-        'maxFeePerGas': w3.to_wei(250, 'gwei'),
-        'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
-        'nonce': nonce,
-        'chainId': ETH_CHAIN_ID})
+        signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
+        tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        self.assertEqual(tx_receipt['status'], 1)
+        print('✅ eth_update_attribute, Success')
+        return tx_receipt['blockNumber']
 
-    signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    if tx_receipt['status'] != 1:
-        print(tx_receipt)
-        raise IOError
-    print('✅ eth_update_attribute, Success')
-    return tx_receipt['blockNumber']
+    def _eth_remove_attribute(self, contract, eth_kp_src, kp_src, key):
+        w3 = self.w3
+        nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
+        tx = contract.functions.remove_attribute(kp_src.public_key, key).build_transaction({
+            'from': eth_kp_src.ss58_address,
+            'gas': GAS_LIMIT,
+            'maxFeePerGas': w3.to_wei(250, 'gwei'),
+            'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
+            'nonce': nonce,
+            'chainId': ETH_CHAIN_ID})
 
+        signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
+        tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        self.assertEqual(tx_receipt['status'], 1)
+        print('✅ eth_remove_attribute, Success')
+        return tx_receipt['blockNumber']
 
-def _eth_remove_attribute(w3, contract, eth_kp_src, kp_src, key):
-    nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
-    tx = contract.functions.remove_attribute(kp_src.public_key, key).build_transaction({
-        'from': eth_kp_src.ss58_address,
-        'gas': GAS_LIMIT,
-        'maxFeePerGas': w3.to_wei(250, 'gwei'),
-        'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
-        'nonce': nonce,
-        'chainId': ETH_CHAIN_ID})
+    def setUp(self):
+        self.w3 = Web3(Web3.HTTPProvider(ETH_URL))
+        self.substrate = SubstrateInterface(url=WS_URL, type_registry=SCALE_CODEC)
 
-    signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-    tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    if tx_receipt['status'] != 1:
-        print(tx_receipt)
-        raise IOError
-    print('✅ eth_remove_attribute, Success')
-    return tx_receipt['blockNumber']
-
-
-def bridge_did_test():
-    w3 = Web3(Web3.HTTPProvider(ETH_URL))
-
-    with SubstrateInterface(url=WS_URL, type_registry=SCALE_CODEC) as substrate:
+    def test_bridge_did(self):
         eth_src = calculate_evm_addr(KP_SRC.ss58_address)
         token_num = 10000 * pow(10, 15)
-        transfer(substrate, KP_SRC, calculate_evm_account(eth_src), token_num)
+        transfer(self.substrate, KP_SRC, calculate_evm_account(eth_src), token_num)
         eth_kp_src = Keypair.create_from_private_key(ETH_PRIVATE_KEY, crypto_type=KeypairType.ECDSA)
-        call_eth_transfer_a_lot(substrate, KP_SRC, eth_src, eth_kp_src.ss58_address.lower())
+        call_eth_transfer_a_lot(self.substrate, KP_SRC, eth_src, eth_kp_src.ss58_address.lower())
 
-        contract = get_contract(w3, DID_ADDRESS, ABI_FILE)
+        contract = get_contract(self.w3, DID_ADDRESS, ABI_FILE)
 
-        block_idx = _eth_add_attribute(w3, contract, eth_kp_src, KP_SRC, KEY, VALUE)
+        block_idx = self._eth_add_attribute(contract, eth_kp_src, KP_SRC, KEY, VALUE)
         data = contract.functions.read_attribute(KP_SRC.public_key, KEY).call()
-        assert(f'0x{data[0].hex()}' == KEY)
-        assert(f'0x{data[1].hex()}' == VALUE)
+        self.assertEqual(f'0x{data[0].hex()}', KEY)
+        self.assertEqual(f'0x{data[1].hex()}', VALUE)
+
         event = contract.events.AddAttribute.create_filter(fromBlock=block_idx, toBlock=block_idx)
         events = event.get_all_entries()
-        assert(f"{events[0]['args']['sender'].upper()}" == f"0X{eth_kp_src.public_key.hex().upper()}")
-        assert(f"{events[0]['args']['did_account'].hex()}" == f"{KP_SRC.public_key.hex()}")
-        assert(f"0x{events[0]['args']['name'].hex()}" == f"{KEY}")
-        assert(f"0x{events[0]['args']['value'].hex()}" == f"{VALUE}")
-        assert(f"{events[0]['args']['validity']}" == f"{VALIDITY}")
+        self.assertEqual(f"{events[0]['args']['sender'].upper()}", f"0X{eth_kp_src.public_key.hex().upper()}")
+        self.assertEqual(f"{events[0]['args']['did_account'].hex()}", f"{KP_SRC.public_key.hex()}")
+        self.assertEqual(f"0x{events[0]['args']['name'].hex()}", f"{KEY}")
+        self.assertEqual(f"0x{events[0]['args']['value'].hex()}", f"{VALUE}")
+        self.assertEqual(f"{events[0]['args']['validity']}", f"{VALIDITY}")
 
-        block_idx = _eth_update_attribute(w3, contract, eth_kp_src, KP_SRC, KEY, NEW_VALUE)
+        block_idx = self._eth_update_attribute(contract, eth_kp_src, KP_SRC, KEY, NEW_VALUE)
         data = contract.functions.read_attribute(KP_SRC.public_key, KEY).call()
-        assert(f'0x{data[0].hex()}' == KEY)
-        assert(f'0x{data[1].hex()}' == NEW_VALUE)
+        self.assertEqual(f'0x{data[0].hex()}', KEY)
+        self.assertEqual(f'0x{data[1].hex()}', NEW_VALUE)
 
         event = contract.events.UpdateAttribute.create_filter(fromBlock=block_idx, toBlock=block_idx)
         events = event.get_all_entries()
-        assert(f"{events[0]['args']['sender'].upper()}" == f"0X{eth_kp_src.public_key.hex().upper()}")
-        assert(f"{events[0]['args']['did_account'].hex()}" == f"{KP_SRC.public_key.hex()}")
-        assert(f"0x{events[0]['args']['name'].hex()}" == f"{KEY}")
-        assert(f"0x{events[0]['args']['value'].hex()}" == f"{NEW_VALUE}")
-        assert(f"{events[0]['args']['validity']}" == f"{VALIDITY}")
+        self.assertEqual(f"{events[0]['args']['sender'].upper()}", f"0X{eth_kp_src.public_key.hex().upper()}")
+        self.assertEqual(f"{events[0]['args']['did_account'].hex()}", f"{KP_SRC.public_key.hex()}")
+        self.assertEqual(f"0x{events[0]['args']['name'].hex()}", f"{KEY}")
+        self.assertEqual(f"0x{events[0]['args']['value'].hex()}", f"{NEW_VALUE}")
+        self.assertEqual(f"{events[0]['args']['validity']}", f"{VALIDITY}")
 
-        block_idx = _eth_remove_attribute(w3, contract, eth_kp_src, KP_SRC, KEY)
-        try:
-            data = contract.functions.read_attribute(KP_SRC.public_key, KEY).call()
-            print('data still can be found')
-            assert(0)
-        except ValueError:
-            pass
+        block_idx = self._eth_remove_attribute(contract, eth_kp_src, KP_SRC, KEY)
+        self.assertRaises(ValueError, contract.functions.read_attribute(KP_SRC.public_key, KEY).call)
 
         event = contract.events.RemoveAttribte.create_filter(fromBlock=block_idx, toBlock=block_idx)
         events = event.get_all_entries()
-        assert(f"{events[0]['args']['did_account'].hex()}" == f"{KP_SRC.public_key.hex()}")
-        assert(f"0x{events[0]['args']['name'].hex()}" == f"{KEY}")
+        self.assertEqual(f"{events[0]['args']['did_account'].hex()}", f"{KP_SRC.public_key.hex()}")
+        self.assertEqual(f"0x{events[0]['args']['name'].hex()}", f"{KEY}")
 
         print('Passssss test_did_bridge')
-
-
-if __name__ == '__main__':
-    bridge_did_test()
