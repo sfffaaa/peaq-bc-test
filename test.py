6e41801b2d739d64b2775c517f85dc6cde94f573
jaypan <pan@peaq.io>
2023-07-28 17:07:16 +0200

    Move bridge_storage_test

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 from tools.two_address_evm_contract_with_extrinsic import evm_extrinsic_test
 from tools.two_address_evm_contract_with_rpc import evm_rpc_test
 from tools.pallet_vesting_test import pallet_vesting_test
-from tools.bridge_storage_test import bridge_storage_test
 import pytest
 
 
@@ -11,4 +10,3 @@ if __name__ == '__main__':
     evm_rpc_test()
     evm_extrinsic_test()
     pallet_vesting_test()
-    bridge_storage_test()
--- tools/bridge_storage_test.py
+++ tools/bridge_storage_test.py
@@ -7,6 +7,8 @@ from tools.utils import WS_URL, ETH_URL, get_eth_chain_id
 from tools.peaq_eth_utils import call_eth_transfer_a_lot, get_contract, generate_random_hex
 from web3 import Web3
 
+import unittest
+
 
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
@@ -20,82 +22,89 @@ KP_SRC = Keypair.create_from_uri('//Alice')
 STORAGE_ADDRESS = '0x0000000000000000000000000000000000000801'
 ETH_PRIVATE_KEY = '0xa2899b053679427c8c446dc990c8990c75052fd3009e563c6a613d982d6842fe'
 ABI_FILE = 'ETH/storage/storage.sol.json'
+TOKEN_NUM = 10000 * pow(10, 15)
+TX_SUCCESS_STATUS = 1
+
+
+def _calcualte_evm_basic_req(substrate, w3, addr):
+    return {
+        'from': addr,
+        'gas': GAS_LIMIT,
+        'maxFeePerGas': w3.to_wei(250, 'gwei'),
+        'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
+        'nonce': w3.eth.get_transaction_count(addr),
+        'chainId': get_eth_chain_id(substrate)
+    }
 
 
 def _eth_add_item(substrate, w3, contract, eth_kp_src, item_type, item):
-    nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
-    tx = contract.functions.add_item(item_type, item).build_transaction({
-        'from': eth_kp_src.ss58_address,
-        'gas': GAS_LIMIT,
-        'maxFeePerGas': w3.to_wei(250, 'gwei'),
-        'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
-        'nonce': nonce,
-        'chainId': get_eth_chain_id(substrate)})
+    tx = contract.functions.add_item(item_type, item).build_transaction(
+        _calcualte_evm_basic_req(substrate, w3, eth_kp_src.ss58_address)
+    )
 
     signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
     tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    if tx_receipt['status'] != 1:
-        print(tx_receipt)
-        raise IOError
-    print('✅ eth_add_item, Success')
-    return tx_receipt['blockNumber']
+    return tx_receipt
 
 
 def _eth_update_item(substrate, w3, contract, eth_kp_src, item_type, item):
-    nonce = w3.eth.get_transaction_count(eth_kp_src.ss58_address)
-    tx = contract.functions.update_item(item_type, item).build_transaction({
-        'from': eth_kp_src.ss58_address,
-        'gas': GAS_LIMIT,
-        'maxFeePerGas': w3.to_wei(250, 'gwei'),
-        'maxPriorityFeePerGas': w3.to_wei(2, 'gwei'),
-        'nonce': nonce,
-        'chainId': get_eth_chain_id(substrate)})
+    tx = contract.functions.update_item(item_type, item).build_transaction(
+        _calcualte_evm_basic_req(substrate, w3, eth_kp_src.ss58_address)
+    )
 
     signed_txn = w3.eth.account.sign_transaction(tx, private_key=eth_kp_src.private_key)
     tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    if tx_receipt['status'] != 1:
-        print(tx_receipt)
-        raise IOError
-    print('✅ eth_update_item, Success')
-    return tx_receipt['blockNumber']
+    return tx_receipt
 
 
-def bridge_storage_test():
-    w3 = Web3(Web3.HTTPProvider(ETH_URL))
+class TestBridgeStorage(unittest.TestCase):
 
-    with SubstrateInterface(url=WS_URL, type_registry=SCALE_CODEC) as substrate:
-        eth_src = calculate_evm_addr(KP_SRC.ss58_address)
-        token_num = 10000 * pow(10, 15)
-        transfer(substrate, KP_SRC, calculate_evm_account(eth_src), token_num)
-        eth_kp_src = Keypair.create_from_private_key(ETH_PRIVATE_KEY, crypto_type=KeypairType.ECDSA)
+    def setUp(self):
+        self._eth_src = calculate_evm_addr(KP_SRC.ss58_address)
+        self._w3 = Web3(Web3.HTTPProvider(ETH_URL))
+        self._substrate = SubstrateInterface(url=WS_URL, type_registry=SCALE_CODEC)
+        self._eth_kp_src = Keypair.create_from_private_key(ETH_PRIVATE_KEY, crypto_type=KeypairType.ECDSA)
+        self._account = calculate_evm_account_hex(self._eth_kp_src.ss58_address)
+
+    def check_item_from_event(self, event, account, item_type, item):
+        events = event.get_all_entries()
+        self.assertEqual(f"0x{events[0]['args']['account'].hex()}", account)
+        self.assertEqual(f"0x{events[0]['args']['item_type'].hex()}", f"{item_type}")
+        self.assertEqual(f"0x{events[0]['args']['item'].hex()}", f"{item}")
+
+    def test_bridge_storage(self):
+        substrate = self._substrate
+        eth_src = self._eth_src
+        w3 = self._w3
+        eth_kp_src = self._eth_kp_src
+        account = self._account
+
+        # setup
+        transfer(substrate, KP_SRC, calculate_evm_account(eth_src), TOKEN_NUM)
         call_eth_transfer_a_lot(substrate, KP_SRC, eth_src, eth_kp_src.ss58_address.lower())
 
         contract = get_contract(w3, STORAGE_ADDRESS, ABI_FILE)
 
-        block_idx = _eth_add_item(substrate, w3, contract, eth_kp_src, ITEM_TYPE, ITEM)
-        account = calculate_evm_account_hex(eth_kp_src.ss58_address)
+        # Execute: Add
+        tx_receipt = _eth_add_item(substrate, w3, contract, eth_kp_src, ITEM_TYPE, ITEM)
+        self.assertEqual(tx_receipt['status'], TX_SUCCESS_STATUS)
+        block_idx = tx_receipt['blockNumber']
+
+        # Cehck
         data = contract.functions.get_item(account, ITEM_TYPE).call()
-        assert(f'0x{data.hex()}' == ITEM)
+        self.assertEqual(f'0x{data.hex()}', ITEM)
         event = contract.events.ItemAdded.create_filter(fromBlock=block_idx, toBlock=block_idx)
-        events = event.get_all_entries()
-        assert(f"0x{events[0]['args']['account'].hex()}" == account)
-        assert(f"0x{events[0]['args']['item_type'].hex()}" == f"{ITEM_TYPE}")
-        assert(f"0x{events[0]['args']['item'].hex()}" == f"{ITEM}")
+        self.check_item_from_event(event, account, ITEM_TYPE, ITEM)
 
-        block_idx = _eth_update_item(substrate, w3, contract, eth_kp_src, ITEM_TYPE, NEW_ITEM)
+        # Executed: Update
+        tx_receipt = _eth_update_item(substrate, w3, contract, eth_kp_src, ITEM_TYPE, NEW_ITEM)
+        self.assertEqual(tx_receipt['status'], TX_SUCCESS_STATUS)
+        block_idx = tx_receipt['blockNumber']
+
+        # Check
         data = contract.functions.get_item(account, ITEM_TYPE).call()
-        assert(f'0x{data.hex()}' == NEW_ITEM)
-
+        self.assertEqual(f'0x{data.hex()}', NEW_ITEM)
         event = contract.events.ItemUpdated.create_filter(fromBlock=block_idx, toBlock=block_idx)
-        events = event.get_all_entries()
-        assert(f"0x{events[0]['args']['account'].hex()}" == account)
-        assert(f"0x{events[0]['args']['item_type'].hex()}" == f"{ITEM_TYPE}")
-        assert(f"0x{events[0]['args']['item'].hex()}" == f"{NEW_ITEM}")
-
-        print('Passssss test_did_bridge')
-
-
-if __name__ == '__main__':
-    bridge_storage_test()
+        self.check_item_from_event(event, account, ITEM_TYPE, NEW_ITEM)
