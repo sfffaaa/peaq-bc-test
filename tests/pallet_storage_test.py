ff9e9b6977c2c7169bc19b7d43ce3c8e9ce7b4cb
jaypan <pan@peaq.io>
2023-09-15 12:01:07 +0200

    Use storage sdk

--- tests/pallet_storage_test.py
+++ tests/pallet_storage_test.py
@@ -2,6 +2,7 @@ import time
 from tools.utils import WS_URL
 from substrateinterface import SubstrateInterface, Keypair
 from peaq.utils import ExtrinsicBatch
+from peaq.storage import storage_add_payload, storage_update_payload, storage_rpc_read
 
 import unittest
 # from tools.pallet_assets_test import pallet_assets_test
@@ -9,12 +10,6 @@ import unittest
 
 def utf8_to_ascii(utf8str):
     return [int(utf8str[i:i+2], 16) for i in range(0, len(utf8str), 2)]
-
-
-def storage_rpc_read(substrate, kp_src, item_type):
-    data = substrate.rpc_request('peaqstorage_readAttribute', [
-                                 kp_src.ss58_address, item_type])
-    return data["result"]["item"]
 
 
 class TestPalletStorage(unittest.TestCase):
@@ -28,17 +23,13 @@ class TestPalletStorage(unittest.TestCase):
         item_type = f'0x{int(time.time())}'
         item = '0x032132'
 
-        batch.compose_call(
-            'PeaqStorage',
-            'add_item',
-            {
-                'item_type': item_type,
-                'item': item,
-            })
+        storage_add_payload(batch, item_type, item)
 
         bl_hash = batch.execute_n_clear()
         self.assertTrue(bl_hash, f'storage_add_item failed: {bl_hash}')
-        self.assertEqual(storage_rpc_read(self._substrate, kp_src, item_type), item)
+        self.assertEqual(
+            storage_rpc_read(self._substrate, kp_src.ss58_address, item_type)['item'],
+            item)
 
     def test_storage_update(self):
         kp_src = Keypair.create_from_uri('//Alice')
@@ -46,27 +37,10 @@ class TestPalletStorage(unittest.TestCase):
         item_type = f'0x{int(time.time())}'
         item = '0x032132'
 
-        batch.compose_call(
-            'PeaqStorage',
-            'add_item',
-            {
-                'item_type': item_type,
-                'item': item,
-            })
-
-        batch.compose_call(
-            'PeaqStorage',
-            'get_item',
-            {
-                'item_type': item_type,
-            })
-
-        batch.compose_call(
-            'PeaqStorage',
-            'update_item',
-            {
-                'item_type': item_type,
-                'item': '0x0123',
-            })
+        storage_add_payload(batch, item_type, item)
+        storage_update_payload(batch, item_type, '0x0123')
         bl_hash = batch.execute_n_clear()
         self.assertTrue(bl_hash, f'storage_update_item failed: {bl_hash}')
+        self.assertEqual(
+            storage_rpc_read(self._substrate, kp_src.ss58_address, item_type)['item'],
+            '0x0123')
