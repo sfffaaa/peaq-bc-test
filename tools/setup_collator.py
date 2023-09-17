18f7d29325204be3141e43a1353fd5de2e99ae71
jaypan <pan@peaq.io>
2023-09-17 22:10:30 +0200

    Add setup collator and snapshot tool

---
+++ tools/setup_collator.py
@@ -0,0 +1,80 @@
+import sys
+sys.path.append('./')
+
+
+from substrateinterface import SubstrateInterface, Keypair
+from peaq.utils import ExtrinsicBatch
+from peaq.sudo_extrinsic import funds
+from tools.utils import PARACHAIN_WS_URL, KP_GLOBAL_SUDO
+import requests
+
+
+def setup_collator(ws_port, rpc_port, kp):
+    substrate = SubstrateInterface(
+        url=f'ws://127.0.0.1:{ws_port}',
+    )
+    aura_key = requests.post(
+        f'http://127.0.0.1:{rpc_port}',
+        headers={'Content-Type': 'application/json'},
+        json={
+            'id': 1,
+            'jsonrpc': '2.0',
+            'method': 'author_rotateKeys',
+            'params': [],
+        }
+    ).json()['result']
+
+    batch = ExtrinsicBatch(substrate, kp)
+    batch.compose_call(
+        'Session', 'set_keys',
+        {
+            'keys': {
+                'aura': aura_key,
+            },
+            'proof': '0x00',
+        })
+    batch.compose_call(
+        'ParachainStaking',
+        'join_candidates',
+        {
+            'stake': 50000 * 10 ** 18,
+        })
+    receipt = batch.execute()
+    print(f'receipt: {receipt}')
+
+
+def fund_addrs(kps):
+    substrate = SubstrateInterface(
+        url=PARACHAIN_WS_URL,
+    )
+    receipt = funds(substrate, KP_GLOBAL_SUDO, [kp.ss58_address for kp in kps], 60000 * 10 ** 18, 0)
+    print(f'funds receipt: {receipt}')
+
+
+if __name__ == '__main__':
+    NUMBER = 12
+    entries = [(10048 + i, 11137 + i, Keypair.create_from_mnemonic(Keypair.generate_mnemonic())) for i in range(0, NUMBER)]
+    fund_addrs([entry[2] for entry in entries])
+    for ws_port, rpc_port, kp in entries:
+        setup_collator(ws_port, rpc_port, kp)
+
+    print('setup actions')
+    substrate = SubstrateInterface(
+        url=PARACHAIN_WS_URL,
+    )
+    batch = ExtrinsicBatch(substrate, KP_GLOBAL_SUDO)
+    batch.compose_sudo_call(
+        'ParachainStaking',
+        'set_max_selected_candidates',
+        {
+            'new': 16
+        })
+
+    batch.execute_n_clear()
+    batch.compose_sudo_call(
+        'ParachainStaking',
+        'force_new_round',
+        {}
+    )
+    batch.execute()
+    batch.execute()
